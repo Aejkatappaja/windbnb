5b4606445b27dbf6a2e6aa43921048b26ad8d16d
Frank Jover <joverjlj@gmail.com>
2023-06-04 00:25:43 +0200

    lost in translation

--- lib/stays.ts
+++ lib/stays.ts
@@ -27,3 +27,38 @@ export async function getAllStays(city?: string) {
 
   return data.json();
 }
+
+// import { NextRequest, NextResponse } from "next/server";
+// import stays from "@/data/stays.json";
+// import { StayType } from "@/types/stay";
+
+// interface FilteredStaysResponse {
+//   stays: StayType[];
+// }
+
+// interface AllStaysResponse {
+//   stays: StayType[];
+// }
+
+// export default async function GET(request: NextRequest): Promise<NextResponse> {
+//   const url = new URL(request.url);
+//   const city = url.searchParams.get("city");
+
+//   // Vérifiez si la ville est présente dans la requête
+//   if (city) {
+//     const filteredStays: StayType[] = stays.filter((stay) => {
+//       if (Array.isArray(stay)) {
+//         return stay.some((s) => s.city === city);
+//       }
+//       return stay.city === city;
+//     });
+
+//     const response: FilteredStaysResponse = { stays: filteredStays };
+//     return NextResponse.json(response);
+//   }
+
+//   // Si la ville n'est pas spécifiée, retournez toutes les données
+//   const allStays: StayType[] = stays;
+//   const response: AllStaysResponse = { stays: allStays };
+//   return NextResponse.json(response);
+// }
